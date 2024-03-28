4f3a649d44b8efff2007b4c71d55458e011a1a98
ponnabook <88271077+ponnabook@users.noreply.github.com>
2024-03-28 18:30:29 +0700

    feat: :sparkles: add pages: Home, Login, Register, Header

---
+++ vacq/models/Hospital.js
@@ -0,0 +1,67 @@
+/** @format */
+
+const mongoose = require('mongoose');
+
+const HospitalSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      require: [true, 'Please add a name'],
+      unique: true,
+      trim: true,
+      maxlength: [50, 'Name can not be more than 50 characters'],
+    },
+    address: {
+      type: String,
+      require: [true, 'Please add an address'],
+    },
+    district: {
+      type: String,
+      require: [true, 'Please add a district'],
+    },
+    province: {
+      type: String,
+      require: [true, 'Please add a province'],
+    },
+    postalcode: {
+      type: String,
+      require: [true, 'Please add a postalcode'],
+      maxlength: [5, 'Postal code can not be more than 5 digits'],
+    },
+    tel: {
+      type: String,
+    },
+    region: {
+      type: String,
+      require: [true, 'Please add a region'],
+    },
+  },
+
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
+
+HospitalSchema.pre(
+  'deleteOne',
+  {
+    document: true,
+    query: false,
+  },
+  async function (next) {
+    console.log(`Appointments being removed from hospital ${this._id}`);
+    await this.model('Appointment').deleteMany({ hospital: this._id });
+    next();
+  }
+);
+
+//Reverse populate with virtuals
+HospitalSchema.virtual('appointments', {
+  ref: 'Appointment',
+  localField: '_id',
+  foreignField: 'hospital',
+  justOne: false,
+});
+
+module.exports = mongoose.model('Hospital', HospitalSchema);
---
+++ vacq/server.js
@@ -0,0 +1,56 @@
+/** @format */
+
+const express = require('express');
+const dotenv = require('dotenv');
+const hospitals = require('./routes/hospitals');
+const auth = require('./routes/auth');
+const appointments = require('./routes/appointments');
+const cookieParser = require('cookie-parser');
+const connectDB = require('./config/db');
+
+//load env vars
+dotenv.config({ path: './config/config.env' });
+
+const app = express();
+
+//body parser
+app.use(express.json());
+
+//connect DB
+connectDB();
+
+app.use('/api/v1/hospitals', hospitals);
+app.use('/api/v1/appointments', appointments);
+app.use('/api/v1/auth', auth);
+app.use(cookieParser());
+
+// app.get("/", (req, res) => {
+//   //1.
+//   //res.send("<h1>Hello from express</h1>");
+//   //2.
+//   //res.send({ name: "Brad" });
+//   //3.
+//   //res.json({ name: "Brad" });
+//   //4.
+//   //res.sendStatus(400);
+//   //5.
+//   //res.status(400).json({ success: false });
+//   res.status(200).json({ success: true, data: { id: 1 } });
+// });
+
+const PORT = process.env.PORT || 5002;
+const server = app.listen(
+  PORT,
+  console.log(
+    'Server running in ',
+    process.env.NODE_ENV,
+    ' made on port ',
+    PORT
+  )
+);
+
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`);
+
+  server.close(() => process.exit(1));
+});
